7265df19011e0ea5d673f35740a40e3f7b12092e
Aditya Hegde <adityavhegde@gmail.com>
2017-07-02 01:00:55 +0530

    basic UI added

---
+++ app.js
@@ -0,0 +1,19 @@
+const menubar = require('menubar');
+const path = require('path')
+//pass the options to create the necessary menu
+//menubar returned object already creates an app instance
+
+const iconPath = path.join(__dirname, '/resources/icon.png');
+const indexPage = path.join(__dirname, '/views/index.html')
+
+const options = {
+	'index': indexPage,
+	'tooltip': 'Walk reminder',
+	'width': 300,
+	'height': 250,
+	'icon': iconPath
+}
+
+
+var menuInstance = new menubar(options);
+
