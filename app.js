f2af1c387194c84e94cd5782058994df9d875bce
Aditya Hegde <adityavhegde@gmail.com>
2017-07-02 12:29:38 +0530

    settings page UI improvements

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const path = require('path')
 //pass the options to create the necessary menu
 //menubar returned object already creates an app instance
 
+/*
+#TODO: add context menu
+*/
 const iconPath = path.join(__dirname, '/resources/icon.png');
 const indexPage = path.join(__dirname, '/views/index.html')
 
@@ -16,4 +19,3 @@ const options = {
 
 
 var menuInstance = new menubar(options);
-
