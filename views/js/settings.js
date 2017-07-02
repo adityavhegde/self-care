60f923f0d4acc9e684e3178a030253a57cfcfca3
Aditya Hegde <adityavhegde@gmail.com>
2017-07-03 00:12:52 +0530

    todo added

--- views/js/settings.js
+++ views/js/settings.js
@@ -1,8 +1,9 @@
 $(document).ready( () => {
     
-    //#TODO: this works in a desktop browser, but not electron
+    //#TODO: this works in a desktop browser, but not in electron app
     $('[data-toggle="tooltip"]').tooltip(); 
 
+    //#TODO: this works in a desktop browser, but not electron in electron app
     $('#edit-day-hours').click(function() {
     	$(this).hide();
     	$(this).next().fadeIn();
