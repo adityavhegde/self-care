fc7a08475e3ca16919fe92ebc7b046fcfa5c7518
Aditya Hegde <adityavhegde@gmail.com>
2017-07-15 10:56:14 -0400

    refactor: code comments

--- views/js/settings.js
+++ views/js/settings.js
@@ -1,9 +1,7 @@
 $(document).ready( () => {
     
-    //#TODO: this works in a desktop browser, but not in electron app
     $('[data-toggle="tooltip"]').tooltip(); 
 
-    //#TODO: this works in a desktop browser, but not electron in electron app
     $('#edit-day-hours').click(function() {
     	$(this).hide();
     	$(this).next().fadeIn();
