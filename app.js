72da3b1993b967bdc3509ac2646ae9a67e66e84c
Aditya Hegde <adityavhegde@gmail.com>
2017-07-28 14:03:29 -0400

    removed unnecessary comments

--- app.js
+++ app.js
@@ -6,9 +6,6 @@ const schedulerInstance = require('./views/js/algorithm');
 //pass the options to create the necessary menu
 //menubar returned object already creates an app instance
 
-/*
-#TODO: add context menu
-*/
 const iconPath = path.join(__dirname, '/resources/icon.png');
 const indexPage = path.join(__dirname, '/views/index.html');
 const productURL = 'https://github.com/adityavhegde/self-care';
